594af95ed1cebc41dfd6f5c1dcf1d5d1ecdd6ee7
lijinxuan <874303470@qq.com>
2021-04-23 00:32:17 +0800

    2021年4月23日00:32:16

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 apt update
 pip3 install flask eventlet flask-socketio
 
+# 复制文件
+cp chatroom-websocket.conf /etc/supervisor/conf.d/chatroom-websocket.conf
+
+# 重启服务器
+supervisorctl restart chatroom-websocket
+
 echo 'succsss'
 echo 'ip'
 hostname -I
